377a867ca06b39511f8f7d8af212450dfc818fa2
XyCrosier <1833272+XyCrosier@users.noreply.github.com>
2025-01-14 23:34:40 -0600

    Ch2 L8 Complete

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from player import Player
 
 def main():
     pygame.init()
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -18,8 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for u in updatables:
+            u.update(dt)
+        for d in drawables:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
